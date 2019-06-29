ab8bda78dfc0326e05aba396da3664dbe2d14bf6
tsengkasing <tsengkasing@gmail.com>
2019-06-30 01:45:06 +0800

    feat(linechart): predict

--- CloudWeb/src/main/resources/static/js/aqi.js
+++ CloudWeb/src/main/resources/static/js/aqi.js
@@ -1,12 +1,115 @@
 function loadAQIFunction() {
 
-    const aqiPredictTitle = 'Predict';
-    const aqiRealTitle = 'Real';
-    const aqiPredictOption = generateGaugeChartOption(aqiPredictTitle, 'aqi-predict');
-    const aqiRealOption = generateGaugeChartOption(aqiRealTitle, 'aqi-real');
+    const AQI_LEVEL = {
+        'Good': 0,
+        'Moderate': 1,
+        'Unhealthy for Sensitive Groups': 2,
+        'Unhealthy': 3,
+        'Very Unhealthy': 4,
+        'Hazardous': 5,
+    };
+    const AQI_LEVEL_EN = [
+        'Good',
+        'Moderate',
+        'Unhealthy for Sensitive Groups',
+        'Unhealthy',
+        'Very Unhealthy',
+        'Hazardous',
+    ];
+    const AQI_LEVEL_ZH = [
+        '好',
+        '中等',
+        '不适于敏感人群',
+        '不健康',
+        '非常不健康',
+        '危险',
+    ];
 
-    const aqiPredictChart = echarts.init(document.getElementById('aqi-predict'));
-    const aqiRealChart = echarts.init(document.getElementById('aqi-real'));
+    const AQI_COLOR = [
+        '#00E400',
+        '#FFFF00',
+        '#FF7E00',
+        '#FF0000',
+        '#8f3f97',
+        '#7E0023'
+    ];
+
+    let acturalAQIPoints = [];
+    let predictAQIPoints = [];
+    let positivePoints = [];
+    let negativePoints = [];
+    let totalPoints = [];
+    let w_positivePoints = [];
+    let w_negativePoints = [];
+    let w_totalPoints = [];
+
+    /**
+     * 计算 AQI 值范围
+     * @param {number} value
+     * @returns {string}
+     */
+    function calculateAQILevel(value) {
+        if (value <= 50) return AQI_LEVEL.Good;
+        if (value <= 100) return AQI_LEVEL.Moderate ;
+        if (value <= 150) return AQI_LEVEL["Unhealthy for Sensitive Groups"];
+        if (value <= 200) return AQI_LEVEL.Unhealthy;
+        if (value <= 300) return AQI_LEVEL["Very Unhealthy"];
+        if (value <= 500) return AQI_LEVEL.Hazardous;
+        throw new Error('Error Value');
+    }
+
+    const setAqiPredictValue = (function () {
+        // const aqiPredictTitle = 'Predict';
+        // const aqiPredictOption = generateGaugeChartOption(aqiPredictTitle, 'aqi-predict');
+        // const aqiPredictChart = echarts.init(document.getElementById('aqi-predict'));
+        const $aqiPredictBG = $('#aqi-predict');
+        const $aqiPredictENText = $('#aqi-predict .en');
+        const $aqiPredictZHText = $('#aqi-predict .zh');
+        const $aqiPredictValueText = $('#aqi-predict .value');
+        /**
+         * @param {number} value
+         */
+        return function (value) {
+            // aqiPredictChart.setOption({
+            //     ...aqiPredictOption,
+            //     series: [{
+            //         ...aqiPredictOption.series[0],
+            //         data: [{name: aqiPredictTitle,value: predict}]
+            //     }]
+            // });
+            const level = calculateAQILevel(value);
+            $aqiPredictBG.css('background-color', AQI_COLOR[level]);
+            $aqiPredictENText.text(AQI_LEVEL_EN[level]);
+            $aqiPredictZHText.text(AQI_LEVEL_ZH[level]);
+            $aqiPredictValueText.text(`AQI: ${value}`);
+        };
+    })();
+    const setAqiRealValue = (function () {
+        // const aqiRealTitle = 'Real';
+        // const aqiRealOption = generateGaugeChartOption(aqiRealTitle, 'aqi-real');
+        // const aqiRealChart = echarts.init(document.getElementById('aqi-predict'));
+        const $aqiRealBG = $('#aqi-real');
+        const $aqiRealENText = $('#aqi-real .en');
+        const $aqiRealZHText = $('#aqi-real .zh');
+        const $aqiRealValueText = $('#aqi-real .value');
+        /**
+         * @param {number} value
+         */
+        return function (value) {
+            // aqiRealChart.setOption({
+            //     ...aqiRealOption,
+            //     series: [{
+            //         ...aqiRealOption.series[0],
+            //         data: [{name: aqiRealTitle,value: real}]
+            //     }]
+            // });
+            const level = calculateAQILevel(value);
+            $aqiRealBG.css('background-color', AQI_COLOR[level]);
+            $aqiRealENText.text(AQI_LEVEL_EN[level]);
+            $aqiRealZHText.text(AQI_LEVEL_ZH[level]);
+            $aqiRealValueText.text(`AQI: ${value}`);
+        };
+    })();
     const setAqiAccuracyValue = (function() {
         const $aqiAccuracyText = document.getElementById('accuracy-value');
         const $aqiAccuracyChart = document.getElementById('accuracy-chart');
@@ -18,26 +121,55 @@ function loadAQIFunction() {
             $aqiAccuracyChart.style.height = `${accuracy + 20}%`;
         };
     })();
+    const setTweetLineChartData = (function () {
+        const tweetLineChartOption = generateCountLineChartOption();
+        const $tweetLineChart = echarts.init(document.getElementById('tweets-line-chart'));
+        return function (values) {
+            predictAQIPoints = mergeAndSortPoints(predictAQIPoints, values);
+            $tweetLineChart.setOption({
+                ...tweetLineChartOption,
+                xAxis: {
+                    type: 'time',
+                },
+                series: [
+                    {
+                        ...tweetLineChartOption.series[0],
+                        data: predictAQIPoints.map(({timestamp, positive}) => ([new Date(timestamp), positive]))
+                    },
+                    {
+                        ...tweetLineChartOption.series[1],
+                        data: predictAQIPoints.map(({timestamp, negative}) => ([new Date(timestamp), negative]))
+                    },
+                    {
+                        ...tweetLineChartOption.series[2],
+                        data: predictAQIPoints.map(({timestamp, total}) => ([new Date(timestamp), total]))
+                    },
+                    {
+                        ...tweetLineChartOption.series[3],
+                        data: predictAQIPoints.map(({timestamp, w_positive}) => ([new Date(timestamp), w_positive]))
+                    },
+                    {
+                        ...tweetLineChartOption.series[4],
+                        data: predictAQIPoints.map(({timestamp, w_negative}) => ([new Date(timestamp), w_negative]))
+                    },
+                    {
+                        ...tweetLineChartOption.series[5],
+                        data: predictAQIPoints.map(({timestamp, w_total}) => ([new Date(timestamp), w_total]))
+                    },
+                ]
+            });
+        };
+    })();
 
     setInterval(async () => {
         const expressions = ['😐', '😢', '😊'];
         try {
-            const { predict, real, accuracy } = await API.fetchAQIStatistic();
-            aqiPredictChart.setOption({
-                ...aqiPredictOption,
-                series: [{
-                    ...aqiPredictOption.series[0],
-                    data: [{name: aqiPredictTitle,value: predict}]
-                }]
-            });
-            aqiRealChart.setOption({
-                ...aqiRealOption,
-                series: [{
-                    ...aqiRealOption.series[0],
-                    data: [{name: aqiRealTitle,value: real}]
-                }]
-            });
-            setAqiAccuracyValue(accuracy);
+            const acturalAQIs = await API.fetchActuralAQI('LONDON', 5);
+            const predictAQIs = await API.fetchPredictAQI('LONDON', 5);
+            setAqiPredictValue(predictAQIs[acturalAQIs.length - 1]['random_tree']);
+            setAqiRealValue(acturalAQIs[acturalAQIs.length - 1]['aqi']);
+            setAqiAccuracyValue(generateRandomInt(100));
+            setTweetLineChartData(predictAQIs);
             window.$lanuchBarrageMulti(
                 new Array(parseInt(Math.random() * 20, 10) )
                     .fill(0)
@@ -60,16 +192,41 @@ function loadAQIFunction() {
 $(document).ready(loadAQIFunction);
 
 const API = {
-    AQI_STATISTIC: '/topic/consumeStatistic',
-    fetchAQIStatistic: async () => {
+    ACTURAL_AQI_BY_CITY: '/api/getActualAqiByCity',
+    PREDICT_AQI_BY_CITY: '/api/getPredictAqiByCity',
+    /**
+     * 获取实际 AQI
+     * @param {string} city
+     * @param {number} limit
+     * @returns {Promise<{predict: number, accuracy: number, real: number}>}
+     */
+    async fetchActuralAQI(city = 'LONDON', limit = 10) {
+        return JSON.parse(`
+        [{"city":"LONDON","timestamp":"2019-06-29 23:00:00","aqi":76},{"city":"LONDON","timestamp":"2019-06-29 22:00:00","aqi":45},{"city":"LONDON","timestamp":"2019-06-29 21:00:00","aqi":41},{"city":"LONDON","timestamp":"2019-06-29 20:00:00","aqi":61},{"city":"LONDON","timestamp":"2019-06-29 19:00:00","aqi":63}]
+        `);
         try {
-            // const response = await fetch(this.AQI_STATISTIC);
-            // const json = response.json();
-            return {
-                predict: generateRandomInt(500),
-                real: generateRandomInt(500),
-                accuracy: generateRandomInt(100),
-            };
+            const response = await fetch(`${this.ACTURAL_AQI_BY_CITY}?city=${city}&limit=${limit}`);
+            const json = await response.json();
+            return json;
+        } catch (err) {
+            console.error(err);
+            throw new Error('[Error] Unable to fetch aqi data');
+        }
+    },
+    /**
+     * 获取预测 AQI
+     * @param {string} city
+     * @param {number} limit
+     * @returns {Promise<{predict: number, accuracy: number, real: number}>}
+     */
+    async fetchPredictAQI(city = 'LONDON', limit = 10) {
+        return JSON.parse(`
+        [{"city":"LONDON","timestamp":"2019-06-30 00:35:00","positive":12,"negative":103,"total":165,"w_positive":0,"w_negative":2,"w_total":3,"random_tree":61.3},{"city":"LONDON","timestamp":"2019-06-30 00:30:00","positive":18,"negative":86,"total":157,"w_positive":0,"w_negative":4,"w_total":5,"random_tree":59.3},{"city":"LONDON","timestamp":"2019-06-30 00:25:00","positive":16,"negative":91,"total":149,"w_positive":1,"w_negative":4,"w_total":7,"random_tree":56.63},{"city":"LONDON","timestamp":"2019-06-30 00:20:00","positive":16,"negative":86,"total":155,"w_positive":0,"w_negative":2,"w_total":2,"random_tree":67.9},{"city":"LONDON","timestamp":"2019-06-30 00:15:00","positive":12,"negative":93,"total":148,"w_positive":0,"w_negative":0,"w_total":2,"random_tree":65.49},{"city":"LONDON","timestamp":"2019-06-30 00:10:00","positive":15,"negative":99,"total":172,"w_positive":1,"w_negative":4,"w_total":5,"random_tree":67.04},{"city":"LONDON","timestamp":"2019-06-30 00:05:00","positive":13,"negative":108,"total":162,"w_positive":2,"w_negative":4,"w_total":6,"random_tree":73.31},{"city":"LONDON","timestamp":"2019-06-30 00:00:00","positive":25,"negative":95,"total":168,"w_positive":0,"w_negative":3,"w_total":4,"random_tree":89.42},{"city":"LONDON","timestamp":"2019-06-29 23:55:00","positive":7,"negative":78,"total":125,"w_positive":0,"w_negative":5,"w_total":5,"random_tree":68.62},{"city":"LONDON","timestamp":"2019-06-29 23:50:00","positive":18,"negative":86,"total":146,"w_positive":0,"w_negative":2,"w_total":2,"random_tree":89.58}]
+        `);
+        try {
+            const response = await fetch(`${this.PREDICT_AQI_BY_CITY}?city=${city}&limit=${limit}`);
+            const json = await response.json();
+            return json;
         } catch (err) {
             console.error(err);
             throw new Error('[Error] Unable to fetch aqi data');
@@ -146,6 +303,49 @@ function generateGaugeChartOption(title, name) {
     };
 }
 
+function generateCountLineChartOption() {
+    return {
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['positive', 'negative', 'total', 'w_positive', 'w_negative', 'w_total']
+        },
+        xAxis: {
+            type: 'time',
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: 'positive',
+                type: 'line',
+            },
+            {
+                name: 'negative',
+                type: 'line',
+            },
+            {
+                name: 'total',
+                type: 'line',
+            },
+            {
+                name: 'w_positive',
+                type: 'line',
+            },
+            {
+                name: 'w_negative',
+                type: 'line',
+            },
+            {
+                name: 'w_total',
+                type: 'line',
+            },
+        ],
+    };
+}
+
 /**
  *
  * @param max {number}
@@ -153,4 +353,27 @@ function generateGaugeChartOption(title, name) {
  */
 function generateRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+/**
+ * 合并去重时间点 并取后 80 个点
+ * @param {Array<object>} currentPoints
+ * @param {Array<object>} comingPoints
+ * @returns {Array<object>}
+ */
+function mergeAndSortPoints(currentPoints, comingPoints) {
+    let points = currentPoints.concat(comingPoints);
+    points.sort(({timestamp: t1}, {timestamp: t2}) => {
+        return new Date(t1).getTime() - new Date(t2).getTime()
+    });
+    let result = [];
+    for (let idx = 0; idx < points.length - 1; ++idx) {
+        if (points[idx].timestamp !== points[idx + 1].timestamp) {
+            result.push(points[idx]);
+        }
+    }
+    if (result.length > 80) {
+        result = result.slice(-80);
+    }
+    return result;
 }
